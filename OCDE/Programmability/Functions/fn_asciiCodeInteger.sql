8ee1e4bc1c7ad15343de215df7829982c1ee1587
compteProjet <64581075+compteProjet@users.noreply.github.com>
2020-05-04 15:03:30 +0200

    ajout fonction

---
+++ OCDE/Programmability/Functions/fn_asciiCodeInteger.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION [dbo].[fn_asciiCodeInteger] (@string AS VARCHAR(max))
+    RETURNS int
+    AS
+    BEGIN
+      DECLARE @result VARCHAR(max);
+      DECLARE @char CHAR(1);
+      DECLARE @position int;
+      SET @position = 1;
+      SET @result = '';
+      WHILE @position <= LEN(@string)
+      BEGIN
+        SET @char = SUBSTRING(@string, @position, 1);
+        SET @result = @result + convert(varchar, ASCII(@char)-31); -- -31 car vrai char commence à 32
+        SET @position = @position + 1;
+      END
+	  --return @Config_Node
+      RETURN (convert(int,@result));
+    END
+GO
