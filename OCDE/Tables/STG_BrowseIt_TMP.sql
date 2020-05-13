a2a21eecd80e02e17f15dd18835f7d19e030ea55
Alona Hruba <64580407+alona-hruba@users.noreply.github.com>
2020-05-13 16:20:34 +0200

    Change STG_Browselt_TMP

--- OCDE/Tables/STG_BrowseIt_TMP.sql
+++ OCDE/Tables/STG_BrowseIt_TMP.sql
@@ -4,7 +4,7 @@ CREATE TABLE [stg].[STG_BrowseIt_TMP](
 	[Heur] [nvarchar](50) NULL,
 	[IP] [nvarchar](50) NULL,
 	[Item] [nvarchar](100) NULL,
-	[Type] [nvarchar](50) NULL,
+	[Type] [nvarchar](2000) NULL,
 	[File_Date] [date] NOT NULL,
-	[Import_Date] [datetime] NOT NULL
+	[Date_Time] DATETIME NOT NULL DEFAULT GETUTCDATE()
 )
