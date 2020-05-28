f2f3b9b14b31e9059d8e32677ea472e2c0948e26
compteProjet <64581075+compteProjet@users.noreply.github.com>
2020-05-28 10:58:11 +0200

    28052020_1

---
+++ Data_Mart/Tables/DIM_Date.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].DIM_Date
+(
+	[PK_Date] DATETIME NOT NULL, 
+    [Date_Name] NVARCHAR(50) NULL, 
+    [Year] DATETIME NULL, 
+    [Year_Name] NVARCHAR(50) NULL, 
+    [Month] DATETIME NULL, 
+    [Month_Name] NVARCHAR(50) NULL, 
+    [Quater] NVARCHAR(53) NULL, 
+    [Day_Of_Year] INT NULL, 
+    [Day_Of_Year_Name] NVARCHAR(50) NULL, 
+    [Day_Of_Month] INT NULL, 
+    [Day_Of_Month_Name] NVARCHAR(50) NULL, 
+    [Month_Of_Year] INT NULL, 
+    [Month_Of_Year_Name] NVARCHAR(50) NULL 
+)
