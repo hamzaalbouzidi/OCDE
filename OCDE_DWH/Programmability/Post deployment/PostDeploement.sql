05f84f5ec62982f7b0ba58cbb2a5f09334574748
Alona Hruba <64580407+alona-hruba@users.noreply.github.com>
2020-05-29 11:39:50 +0200

    Hours creation (Dim_Hours in DWH and stored procedures SP_Hours)

--- OCDE_DWH/Programmability/Post deployment/PostDeploement.sql
+++ OCDE_DWH/Programmability/Post deployment/PostDeploement.sql
@@ -1,1 +1,2 @@
 ﻿EXEC [dwh].[SP_Calendar] '2019-01-01', '2020-12-31'
+EXEC [dwh].[SP_Hours]
---
+++ OCDE_DWH/Table/DIM_Hours.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dwh].[DIM_Hours]
+(
+	[Download_hour] INT NULL, 
+    [Download_Time] TIME NULL 
+)
